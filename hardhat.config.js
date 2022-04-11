e8cc845496163c0ccbf4d3eb49e5df104a2bef8c
Hoang Vu <vviethoang@gmail.com>
2022-04-11 08:56:32 +0700

    chore: configure with testnet account

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/FokIlewLe9aFM-If4LX3M3Yff_YHI2UP",
+      accounts: ["6c93c3a7f18ec5270635bc039a1dff3ec1a1a33e2d1590981ef3d1aa5826d71a"]
+    },
+  },
+};
+
